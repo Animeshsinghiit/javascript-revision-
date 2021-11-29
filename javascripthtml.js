2e58d38830712d2235436e567b58ad52c04a1875
Animesh Singh <animeshsingh.iit@gmail.com>
2021-11-30 00:03:49 +0530

    javascript d1

---
+++ javascripthtml.js
@@ -0,0 +1,11 @@
+console.log("Welcome")
+let a=document;
+a=document.all;
+// a=document.body;
+// a=document.forms;
+// Array.from(a).forEach(function(element){ //a ki madad se bna lo ek array
+//     console.log(element) 
+// // })
+// a=document.links; //it makes list of all links
+// a=document.links[0].href; //Gives the source link of 0th index link.
+console.log(a)
