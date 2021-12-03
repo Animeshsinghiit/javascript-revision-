914b95da1180d488cc00ae58376f2fc1b1602846
Animesh Singh <animeshsingh.iit@gmail.com>
2021-12-03 14:11:16 +0530

    studied single element selector

--- javascripthtml.js
+++ javascripthtml.js
@@ -1,11 +1,39 @@
 console.log("Welcome")
-let a=document;
-a=document.all;
-// a=document.body;
-// a=document.forms;
-// Array.from(a).forEach(function(element){ //a ki madad se bna lo ek array
-//     console.log(element) 
-// // })
-// a=document.links; //it makes list of all links
-// a=document.links[0].href; //Gives the source link of 0th index link.
-console.log(a)
+// let a=document;
+// a=document.all;
+// // a=document.body;
+// // a=document.forms;
+// // Array.from(a).forEach(function(element){ //a ki madad se bna lo ek array
+// //     console.log(element) 
+// // // })
+// // a=document.links; //it makes list of all links
+// // a=document.links[0].href; //Gives the source link of 0th index link.
+// console.log(a)
+
+/*
+we have two kinds of selectors.
+1-single element selector
+2-multi element selector
+
+*/
+//Single Element selector 
+// let element=document.getElementById("myfirst") // gives you element with id = myfirst
+// // element=element.className // it will tell us the class of element selected with id= myfirst
+// // element=element.childNodes // it will give all the child node present in element with given id.
+// // element=element.parentNode // it will give us parent node of given element.(kis div tage me present hai)
+// // element.style.color="red" //using it we can change css of given element using js.
+// // element.innerText="animesh singh" // it changes the inner text of the given element
+// element.innerHTML="<b>animesh singh is good boy</b>"// it changes the inner html of a given element
+// console.log(element)
+
+// Query Selector
+// let sel=document.querySelector("#myfirst")//it gives element with id = myfirst
+// // sel=document.querySelector(".child1") // it gives an element with class= child1
+// // sel=document.querySelector("div")//gives an element with tag= div(in case of div we get first div)
+// // sel.style.color="green" // changes the css of sel
+// console.log(sel)
+
+/*
+multielement selector--------------------------------------------------------------
+*/
+
